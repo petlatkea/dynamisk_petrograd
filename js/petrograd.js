2cf3f24ce3b8475bd8a307818684cf0a5d0a52b0
Peter Lind <petl@MacBook-Pro-2.local>
2017-02-23 21:15:48 +0100

    add modal template

--- js/petrograd.js
+++ js/petrograd.js
@@ -44,6 +44,66 @@ function visProdukt( produkt ) {
         klon.querySelector(".pris").classList.add("rabat");
     }
 
+    if( produkt.vegetar == true ) {
+        klon.querySelector(".produkt").classList.add("vegetar");
+    }
+
+    // tilføj produkt-id til modalknap
+    klon.querySelector(".modalknap").dataset.produkt = produkt.id;
+
+    // registrer klik på modalknap
+    klon.querySelector(".modalknap").addEventListener("click", modalKnapKlik)
+
     // append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
 }
+
+function modalKnapKlik( event ) {
+    console.log("knapklik", event);
+
+    // find det produkt id, hvis knap der blev trykket på
+    var produktId = event.target.dataset.produkt;
+    console.log("Klik på produkt: ", produktId);
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/product?callback=?",{id: produktId}, visModalProdukt);
+
+}
+
+function visModalProdukt( produkt ) {
+    console.log("vis modal for ", produkt);
+
+    // find modal_template - klon den
+    var klon = document.querySelector("#modal_template").content.cloneNode(true);
+
+    // put data i klonen
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+
+    // sletter det der stod i modal-content
+    document.querySelector(".modal-content").innerHTML = "";
+
+    // append klonen til modal-content
+    document.querySelector(".modal-content").appendChild(klon);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
